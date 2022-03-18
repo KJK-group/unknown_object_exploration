f2a3d0179c6e18f534746c985cba1ca69f7f7a61
Jens <jens.jens@live.dk>
2022-03-18 13:01:34 +0100

    feat(trajectory_follower): structure set up

--- src/trajectory_follower.cpp
+++ src/trajectory_follower.cpp
@@ -11,11 +11,16 @@
 #include <tuple>
 
 #define TOLERANCE 0.2
+#define A 0.4
+#define B 0.6
+#define C -1.4
+#define D -0.6
+#define E 1.6
+#define F -1.5
 
 using namespace std;
 
 ros::Publisher pub_velocity;
-ros::Publisher pub_position;
 
 ros::Subscriber sub_state;
 ros::Subscriber sub_odom;
@@ -25,9 +30,24 @@ ros::ServiceClient client_mode;
 
 // state variables
 mavros_msgs::State state;
-auto reached_altitude = false;
 
+// targets
 auto desired_altitude = 5.f;
+auto subject_center = make_tuple<float, float, float>(0.0f, 0.0f, move(desired_altitude));
+
+// controller gains
+auto k_alpha = 1.f;
+auto k_rho = 1.f;
+
+// trajectory function
+auto trajectory(float x) -> float {
+    return A * pow(x, 5) + B * pow(x, 4) + C * pow(x, 3) + D * pow(x, 2) + E * x + F;
+}
+
+// trajectory slope
+auto trajectory_slope(float x) -> float {
+    return 5 * A * pow(x, 4) + 4 * B * pow(x, 3) + 3 * pow(x, 2) + D * x + E;
+}
 
 auto odom_cb(const nav_msgs::Odometry::ConstPtr& msg) -> void {
     // current position
@@ -35,9 +55,26 @@ auto odom_cb(const nav_msgs::Odometry::ConstPtr& msg) -> void {
     // current yaw
     auto yaw = tf2::getYaw(msg->pose.pose.orientation);
 
-    // altitude error
-    auto error_alt = abs(desired_altitude - pos.z);
-    ROS_INFO("altitude error: %f.5", error_alt);
+    // heading error
+    auto desired_heading = M_PI / 2;
+    auto error_heading = desired_heading - yaw;
+    // correct for magnitude larger than π
+    if (error_heading > M_PI) {
+        error_heading - 2 * M_PI;
+    } else if (error_heading < -M_PI) {
+        error_heading + 2 * M_PI;
+    }
+
+    // position errors
+    // y
+    auto error_y = trajectory(pos.x) - pos.y;
+    // z - altitude error
+    auto error_alt = desired_altitude - msg->pose.pose.position.z;
+
+    // controller
+    auto omega =
+
+        ROS_INFO("altitude error: %f.5", error_alt);
 
     reached_altitude = (error_alt < TOLERANCE);
 }
@@ -58,8 +95,6 @@ auto main(int argc, char** argv) -> int {
     // velocity publisher
     pub_velocity =
         nh.advertise<geometry_msgs::TwistStamped>("/mavros/setpoint_velocity/cmd_vel", 10);
-    // position publisher
-    pub_position = nh.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_position/local", 10);
 
     // arm service client
     client_arm = nh.serviceClient<mavros_msgs::CommandBool>("/mavros/cmd/arming");
@@ -72,44 +107,31 @@ auto main(int argc, char** argv) -> int {
         rate.sleep();
     }
 
-    while (ros::ok()) {
-        // arm the drone
-        if (!state.armed) {
-            mavros_msgs::CommandBool srv;
-            srv.request.value = true;
-            if (client_arm.call(srv)) {
-                ROS_INFO("throttle armed: success");
-            } else {
-                ROS_WARN("throttle armed: fail");
-            }
-        }
-
-        // set drone mode to OFFBOARD
-        if (state.mode != "OFFBOARD") {
-            mavros_msgs::SetMode mode_msg;
-            mode_msg.request.custom_mode = "OFFBOARD";
-
-            if (client_mode.call(mode_msg) && mode_msg.response.mode_sent) {
-                ROS_INFO("mode set: OFFBOARD");
-            } else {
-                ROS_WARN("mode set: fail");
-            }
-        }
-
-        // fly to pose
-        // then fly in a direction
-        if (!reached_altitude) {
-            geometry_msgs::PoseStamped pose;
-            pose.pose.position.z = desired_altitude;
-            pub_position.publish(pose);
-
-            // ROS_INFO("flying to pose");
+    // arm the drone
+    if (!state.armed) {
+        mavros_msgs::CommandBool srv;
+        srv.request.value = true;
+        if (client_arm.call(srv)) {
+            ROS_INFO("throttle armed: success");
         } else {
-            geometry_msgs::TwistStamped command;
-            command.twist.linear.x = 1;
-            pub_velocity.publish(command);
+            ROS_WARN("throttle armed: fail");
         }
+    }
 
+    // set drone mode to OFFBOARD
+    if (state.mode != "OFFBOARD") {
+        mavros_msgs::SetMode mode_msg;
+        mode_msg.request.custom_mode = "OFFBOARD";
+
+        if (client_mode.call(mode_msg) && mode_msg.response.mode_sent) {
+            ROS_INFO("mode set: OFFBOARD");
+        } else {
+            ROS_WARN("mode set: fail");
+        }
+    }
+
+    // ROS spin
+    while (ros::ok()) {
         ros::spinOnce();
         rate.sleep();
     }
