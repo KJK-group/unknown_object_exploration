5d718722c065130abe4b6c0d5ef7a9f213b26eee
Jens <jens.jens@live.dk>
2022-03-23 15:58:31 +0100

    feat(trajectory_follower): 3D trajectory, transform not working

--- src/trajectory_follower.cpp
+++ src/trajectory_follower.cpp
@@ -1,3 +1,4 @@
+#include <geometry_msgs/PointStamped.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <mavros_msgs/CommandBool.h>
@@ -6,14 +7,16 @@
 #include <nav_msgs/Odometry.h>
 #include <ros/ros.h>
 #include <tf2/utils.h>
+#include <tf2_eigen/tf2_eigen.h>
 #include <tf2_ros/transform_listener.h>
 
 #include <cmath>
 #include <eigen3/Eigen/Dense>
 #include <tuple>
 
-#define V_MAX 5
-#define TOLERANCE 0.2
+#include "boost/format.hpp"
+
+#define V_MAX 0.1
 #define A 0.4
 #define B 0.6
 #define C -1.4
@@ -22,7 +25,15 @@
 #define F -1.5
 
 using namespace std;
+using boost::format;
+using boost::io::group;
 using Eigen::Vector2f;
+using Eigen::Vector3f;
+
+// colors
+auto magenta = "\u001b[35m";
+auto green = "\u001b[32m";
+auto reset = "\u001b[0m";
 
 // publishers
 ros::Publisher pub_velocity;
@@ -35,34 +46,46 @@ ros::Subscriber sub_odom;
 ros::ServiceClient client_arm;
 ros::ServiceClient client_mode;
 
+// time
 ros::Time start_time;
 
 // transform utilities
 tf2_ros::Buffer tf_buffer;
 
+// sequence counters
+auto seq_from = 0;
+auto seq_to = 0;
+
 // state variables
 mavros_msgs::State state;
-Vector2f direction_vec;
 
 // targets
-auto desired_altitude = 5.f;
-auto subject_center = make_tuple(0.0f, 0.0f, move(desired_altitude));
+auto altitude_offset = 5.f;
+auto subject_center = make_tuple(0.0f, 0.0f, move(altitude_offset));
 
 // controller gains
-auto k_alpha = 10.f;
+auto k_alpha = 1.f;
 auto k_rho = 1.f;
 
-// trajectory function
+// 5th order trajectory function
 auto trajectory(float x) -> float {
     return A * pow(x, 5) + B * pow(x, 4) + C * pow(x, 3) + D * pow(x, 2) + E * x + F;
 }
 
-// trajectory slope
+// 5th order trajectory slope
 auto trajectory_slope(float x) -> float {
     return 5 * A * pow(x, 4) + 4 * B * pow(x, 3) + 3 * pow(x, 2) + D * x + E;
 }
 
-auto circle_trajectory(float t) -> Vector2f { return Vector2f(4 * cos(t), 4 * sin(t)); }
+// circle vector function
+auto circle_trajectory(float t) -> Vector2f {
+    return Vector2f(4 * cos(V_MAX * t), 4 * sin(V_MAX * t));
+}
+
+// 3D trajectory
+auto circle_trajectory_3d(float t) -> Vector3f {
+    return Vector3f(4 * cos(V_MAX * t), 4 * sin(V_MAX * t), cos(t));
+}
 
 auto odom_cb(const nav_msgs::Odometry::ConstPtr& msg) -> void {
     // current position
@@ -82,15 +105,42 @@ auto odom_cb(const nav_msgs::Odometry::ConstPtr& msg) -> void {
         error_heading + 2 * M_PI;
     }
 
+    // lookup transform
+    auto frame_world = "PX4";
+    auto frame_body = "PX4/odom_local_ned";
+    geometry_msgs::TransformStamped transform;
+    try {
+        // transform from px4 drone odom to px4 world
+        transform = tf_buffer.lookupTransform(frame_world, frame_body, ros::Time(0));
+        ROS_INFO_STREAM(magenta << "transform:\n" << transform << reset);
+    } catch (tf2::TransformException& ex) {
+        ROS_INFO("%s", ex.what());
+        ros::Duration(1.0).sleep();
+    }
+
+    // get expected position
+    auto expected_pos = circle_trajectory_3d(delta_time);
+    // transform expected_pos to body frame
+    // point in world frame "PX4"
+    auto from = geometry_msgs::PointStamped();
+    from.header.seq = seq_from++;
+    from.header.stamp = ros::Time::now();
+    from.header.frame_id = frame_world;
+    from.point.x = expected_pos(0);
+    from.point.y = expected_pos(1);
+    from.point.z = expected_pos(2);
+    // point in body frame "PX4/odom_local_ned"
+    auto body = geometry_msgs::PointStamped();
+    body.header.seq = seq_to++;
+    body.header.stamp = ros::Time::now();
+    body.header.frame_id = frame_body;
+    // apply transform giving reference to body point
+    tf2::doTransform(body, from, transform);
+
     // position errors
-    auto expected_pos = circle_trajectory(delta_time);
     auto error_x = expected_pos(0) - pos.x;
     auto error_y = expected_pos(1) - pos.y;
-    auto error_z = desired_altitude - msg->pose.pose.position.z;
-
-    // lookup transform
-    // try {
-    // }
+    auto error_z = expected_pos(2) + altitude_offset - pos.z;
 
     // controller
     auto omega = k_alpha * error_heading;
@@ -107,17 +157,20 @@ auto odom_cb(const nav_msgs::Odometry::ConstPtr& msg) -> void {
     pub_velocity.publish(command);
 
     // logging for debugging
-    ROS_WARN("errors:");
-    ROS_INFO("  x: %.5f", error_x);
-    ROS_INFO("  y: %.5f", error_y);
-    ROS_INFO("  z: %.5f", error_z);
-    ROS_WARN("controller outputs:");
-    ROS_INFO("  omega: %.5f", omega);
-    ROS_INFO("  x_vel: %.5f", x_vel);
-    ROS_INFO("  y_vel: %.5f", y_vel);
-    ROS_INFO("  z_vel: %.5f", z_vel);
-    ROS_WARN("time:");
-    ROS_INFO("  delta_time: %.5f", delta_time);
+    ROS_INFO_STREAM(magenta << "from pose:\n" << from << reset);
+    ROS_INFO_STREAM(magenta << "to pose:\n" << body << reset);
+    // standard state logging
+    ROS_INFO_STREAM(green << "errors:" << reset);
+    ROS_INFO_STREAM("  y: " << format("%1.5f") % group(setfill(' '), setw(8), error_y));
+    ROS_INFO_STREAM("  x: " << format("%1.5f") % group(setfill(' '), setw(8), error_x));
+    ROS_INFO_STREAM("  z: " << format("%1.5f") % group(setfill(' '), setw(8), error_z));
+    ROS_INFO_STREAM(green << "controller outputs:" << reset);
+    ROS_INFO_STREAM("  omega: " << format("%1.5f") % group(setfill(' '), setw(8), omega));
+    ROS_INFO_STREAM("  x_vel: " << format("%1.5f") % group(setfill(' '), setw(8), x_vel));
+    ROS_INFO_STREAM("  y_vel: " << format("%1.5f") % group(setfill(' '), setw(8), y_vel));
+    ROS_INFO_STREAM("  z_vel: " << format("%1.5f") % group(setfill(' '), setw(8), z_vel));
+    ROS_INFO_STREAM(green << "time:" << reset);
+    ROS_INFO_STREAM("  delta_time: " << format("%1.2f") % group(setfill(' '), setw(5), delta_time));
 }
 
 auto state_cb(const mavros_msgs::State::ConstPtr& msg) -> void { state = *msg; }
