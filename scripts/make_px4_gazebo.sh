ea64b94dcce1f0a7b7b02bc4a46ebc70448da5aa
Jens <jens.jens@live.dk>
2022-03-23 17:01:54 +0100

    feat(launch): gazebo launch file

---
+++ scripts/make_px4_gazebo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#xdotool key ctrl+alt+minus
+#cd ~/airsim_ws/PX4-Autopilot
+#make px4_sitl_default none_iris
+terminator --new-tab -e "cd ../../PX4-Autopilot && make px4_sitl_default gazbo"
+#terminator_pid=$!
+#trap "kill -9 $terminator_pid" EXIT
+#trap "kill -9 $terminator_pid" SIGINT
