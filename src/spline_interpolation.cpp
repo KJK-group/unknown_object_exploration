02e37239f09c37e7169b7b793a8cb48ca134b52b
Jens <jens.jens@live.dk>
2022-03-29 15:58:00 +0200

    feat(planner): conversion to using vector and Vector3f

--- src/spline_interpolation.cpp
+++ src/spline_interpolation.cpp
@@ -1,14 +1,20 @@
 #include <tf2_eigen/tf2_eigen.h>
 
 #include <cmath>
+#include <utility>
+#include <vector>
+
+using Eigen::Vector3f;
+using std::pair;
+using std::vector;
 
 class BezierSpline {
    public:
-    BezierSpline(float*** points);
+    BezierSpline(vector<Vector3f> points);
     auto get_point(float time) -> float;
 
    private:
-    float** LUT;
+    vector<pair<float, float>> pointsLUT;
     float arc_length;
     auto get_time(float distance) -> float;
 };
