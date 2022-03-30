0290ff6bfa3d17afb6fd8301db4bf3946c81a317
Jens <jens.jens@live.dk>
2022-03-30 14:52:14 +0200

    chore(utils): include utils folder

---
+++ include/multi_drone_inspection/utils/math.hpp
@@ -0,0 +1,15 @@
+#ifndef _MULTI_DRONE_INSPECTION_MATH_HPP_
+#define _MULTI_DRONE_INSPECTION_MATH_HPP_
+
+namespace mdi::utils {
+//--------------------------------------------------------------------------------------------------
+// Return the factorial of `i`
+auto factorial(unsigned int i) -> unsigned int;
+
+//--------------------------------------------------------------------------------------------------
+// Returns the binomial coefficient for `n` choose `i`,
+// using the factorial function above
+auto binomial_coefficient(int n, int i) -> int;
+}  // namespace mdi::utils
+
+#endif  // _MULTI_DRONE_INSPECTION_MATH_HPP_
