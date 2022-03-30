973a0cba8b15de5c8c2db18de7d99ed5fc574869
JensHJ <jens.jens@live.dk>
2022-03-30 21:19:29 +0200

    feat(BezierSpline): arc length calculation

--- src/bezier_spline.cpp
+++ src/bezier_spline.cpp
@@ -39,6 +39,17 @@ auto BezierSpline::generate_spline(vector<Vector3f> points, int resolution) -> v
 }
 
 //--------------------------------------------------------------------------------------------------
+// Approximates the arc length of the spline from the calculated points along the spline
+auto BezierSpline::approximate_arc_length() -> void {
+    auto arc_length = 0;
+    for (int p = 1; p < spline_points.size(); p++) {
+        auto diff = spline_points[i-1] - spline_points[i];
+        arc_length += diff.norm();
+    }
+    this->arc_length = arc_length;
+}
+
+//--------------------------------------------------------------------------------------------------
 // Generates the binomial LUT for a Bezier pline with `n` point
 // This will be used to generate the spline
 auto BezierSpline::generate_binomial_lut(int n) -> void {
@@ -47,10 +58,14 @@ auto BezierSpline::generate_binomial_lut(int n) -> void {
     }
 }
 
+//--------------------------------------------------------------------------------------------------
+// Returns the closest point at the given `time`, where 0<=time<=1.
 auto BezierSpline::get_point_at_time(float time) -> Vector3f {
     assert(time <= 1.0 && time >= 0);
     return this->spline_points[(int)(this->spline_points.size() * time)];
 }
 
+//--------------------------------------------------------------------------------------------------
+// Returns a vector of all points along the spline
 auto BezierSpline::get_spline_points() -> vector<Vector3f> { return this->spline_points; }
 }  // namespace mdi
