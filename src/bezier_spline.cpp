3e3800f13fab29907e856466a3d68eae2f907385
Jens <jens.jens@live.dk>
2022-03-30 14:53:04 +0200

    feat(BezierSpline): Bezier spline working

--- src/bezier_spline.cpp
+++ src/bezier_spline.cpp
@@ -1,5 +1,7 @@
 #include "multi_drone_inspection/bezier_spline.hpp"
 
+#include <iostream>
+
 namespace mdi {
 //--------------------------------------------------------------------------------------------------
 // Constructor; generates the spline the first time,
@@ -15,29 +17,36 @@ BezierSpline::BezierSpline(vector<Vector3f> points, int resolution) {
 auto BezierSpline::generate_spline(vector<Vector3f> points, int resolution) -> void {
     this->resolution = resolution;
     auto n = points.size();
-
     // in case the binomial lookup table isn't big enough, generate a new one
     if (binomial_lut.size() != n) {
         generate_binomial_lut(n);
     }
+    assert(points.size() == binomial_lut.size());
 
     // let t run through [0;1] with steps defined by the resolution
-    for (float t = 0; t < 1; t += 1 / resolution) {
+    for (int t = 0; t <= 1 * resolution; t += 1) {
         // sum up all bezier terms, using the explicit definition given by:
         // https://en.wikipedia.org/wiki/B%C3%A9zier_curve
+        auto time = (float)t / (float)resolution;
         auto spline_point = Vector3f(0, 0, 0);
+        std::cout << "time: " << time << std::endl;
         for (int i = 0; i < n; i++) {
-            spline_point += binomial_lut[i] * pow((1 - t), n) * points[i];
+            std::cout << "points[" << i << "] = [" << points[i](0) << " " << points[i](1) << " "
+                      << points[i](2) << "]"
+                      << "\t";
+            std::cout << "binomial_lut[" << i << "] = " << binomial_lut[i] << "\t";
+
+            auto w = binomial_lut[i] * pow(time, i) * pow(1 - time, n - i);
+
+            std::cout << "w for " << i << " = " << w << std::endl;
+
+            spline_point += w * points[i];
         }
+        std::cout << "resulting point = [" << spline_point(0) << " " << spline_point(1) << " "
+                  << spline_point(2) << "]" << std::endl;
+
         spline_points.push_back(spline_point);
     }
-}
-
-//--------------------------------------------------------------------------------------------------
-// Returns the binomial coefficient for `n` choose `i`,
-// using the factorial function from the boost library
-auto BezierSpline::binomial_coefficient(int n, int i) -> int {
-    return factorial<int>(n) / (factorial<int>(i) * factorial<int>(n - i));
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -45,7 +54,7 @@ auto BezierSpline::binomial_coefficient(int n, int i) -> int {
 // This will be used to generate the spline
 auto BezierSpline::generate_binomial_lut(int n) -> void {
     for (int i = 0; i < n; i++) {
-        binomial_lut.push_back(binomial_coefficient(n, i));
+        binomial_lut.push_back(mdi::utils::binomial_coefficient(n, i));
     }
 }
 
@@ -53,4 +62,6 @@ auto BezierSpline::get_point_at_time(float time) -> Vector3f {
     assert(time <= 1.0 && time >= 0);
     return this->spline_points[(int)(this->spline_points.size() * time)];
 }
+
+auto BezierSpline::get_spline_points() -> vector<Vector3f> { return this->spline_points; }
 }  // namespace mdi
--- src/spline_tester.cpp
+++ src/spline_tester.cpp
@@ -1,6 +1,8 @@
 #include <geometry_msgs/PointStamped.h>
 #include <ros/ros.h>
 
+#include <algorithm>
+
 #include "multi_drone_inspection/bezier_spline.hpp"
 
 using mdi::BezierSpline;
@@ -8,8 +10,10 @@ using mdi::BezierSpline;
 ros::Publisher pub_point;
 
 auto duration = 5.f;
+auto seq = 0;
 
 auto main(int argc, char** argv) -> int {
+    ROS_INFO_STREAM("START");
     ros::init(argc, argv, "mdi_spline_tester");
     auto nh = ros::NodeHandle();
     ros::Rate rate(20.0);
@@ -20,14 +24,51 @@ auto main(int argc, char** argv) -> int {
     auto points =
         vector<Vector3f>{Vector3f(0.0, 0.0, 0.0), Vector3f(1.0, 0.5, 0.2), Vector3f(0.5, 1.5, 0.3),
                          Vector3f(0.8, 2.0, 0.7), Vector3f(1.2, 2.2, 1.5), Vector3f(1.0, 3.0, 1.0)};
+    std::reverse(points.begin(), points.end());
+    ROS_INFO_STREAM("Before constructor");
     auto spline = BezierSpline(points);
 
-    auto delta_time = ros::Duration();
-    while (ros::ok() && delta_time.toSec() < duration) {
-        auto delta_time = start_time - ros::Time::now();
-        spline.get_point_at_time((float)(delta_time.toSec() / duration));
+    auto spline_points = spline.get_spline_points();
+    for (int i = 0; i < spline_points.size(); i++) {
+        // ROS_INFO_STREAM("point:\n");
+        // ROS_INFO_STREAM("  x: " << spline_points[i](0));
+        // ROS_INFO_STREAM("  y: " << spline_points[i](1));
+        // ROS_INFO_STREAM("  z: " << spline_points[i](2));
+
+        geometry_msgs::PointStamped p;
+        p.header.seq = seq++;
+        p.header.frame_id = "map";
+        p.header.stamp = ros::Time::now();
+
+        p.point.x = spline_points[i](0);
+        p.point.y = spline_points[i](1);
+        p.point.z = spline_points[i](2);
+        pub_point.publish(p);
         ros::spinOnce();
         rate.sleep();
     }
+    auto delta_time = ros::Duration();
+    // while (ros::ok() && delta_time.toSec() < duration) {
+    //     auto delta_time = start_time - ros::Time::now();
+    //     auto point = spline.get_point_at_time((float)(delta_time.toSec() / duration));
+    //     // ROS_INFO_STREAM("point:\n");
+    //     // ROS_INFO_STREAM("  x: " << point(0));
+    //     // ROS_INFO_STREAM("  y: " << point(1));
+    //     // ROS_INFO_STREAM("  z: " << point(2));
+
+    //     // message
+    //     geometry_msgs::PointStamped p;
+    //     p.header.seq = seq++;
+    //     p.header.frame_id = "map";
+    //     p.header.stamp = ros::Time::now();
+
+    //     p.point.x = point(0);
+    //     p.point.y = point(1);
+    //     p.point.z = point(2);
+
+    //     pub_point.publish(p);
+    //     ros::spinOnce();
+    //     rate.sleep();
+    // }
     return 0;
 }
