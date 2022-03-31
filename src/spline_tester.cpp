04a8a3f26a7baf0544b647a3e7752b15e6a07038
Jens <jens.jens@live.dk>
2022-03-31 12:00:11 +0200

    chore(BezierSpline): shared variable split

--- src/spline_tester.cpp
+++ src/spline_tester.cpp
@@ -12,7 +12,8 @@ ros::Publisher pub_spline;
 ros::Publisher pub_spline_input;
 
 auto duration = 5.f;
-auto seq = 0;
+auto input_seq = 0;
+auto output_seq = 0;
 
 auto main(int argc, char** argv) -> int {
     ROS_INFO_STREAM("START");
@@ -28,8 +29,10 @@ auto main(int argc, char** argv) -> int {
     auto points =
         vector<Vector3f>{Vector3f(0.0, 0.0, 0.0), Vector3f(1.0, 0.5, 0.2), Vector3f(0.5, 1.5, 0.3),
                          Vector3f(0.8, 2.0, 0.7), Vector3f(1.2, 2.2, 1.5), Vector3f(1.0, 3.0, 1.0)};
+
     auto input_points = vector<geometry_msgs::Point>();
     for (auto& point : points) {
+        point *= 10;
         geometry_msgs::Point p;
         p.x = point(0);
         p.y = point(1);
@@ -37,20 +40,20 @@ auto main(int argc, char** argv) -> int {
         ROS_INFO_STREAM("input point: " << p);
         input_points.push_back(p);
     }
-    visualization_msgs::Marker m;
-    m.header.seq = seq++;
-    m.header.frame_id = "map";
-    m.header.stamp = ros::Time();
-    m.type = visualization_msgs::Marker::LINE_STRIP;
-    m.color.a = 1.0;
-    m.color.r = 0.0;
-    m.color.g = 1.0;
-    m.color.b = 1.0;
-    m.points = input_points;
-    m.scale.x = 0.1;
-    m.scale.y = 0.1;
-    m.scale.z = 0.1;
-    pub_spline_input.publish(m);
+    visualization_msgs::Marker input_line_marker;
+    input_line_marker.header.seq = input_seq++;
+    input_line_marker.header.frame_id = "map";
+    input_line_marker.header.stamp = ros::Time();
+    input_line_marker.type = visualization_msgs::Marker::LINE_STRIP;
+    input_line_marker.color.a = 1.0;
+    input_line_marker.color.r = 0.0;
+    input_line_marker.color.g = 1.0;
+    input_line_marker.color.b = 1.0;
+    input_line_marker.points = input_points;
+    input_line_marker.scale.x = 0.1;
+    input_line_marker.scale.y = 0.1;
+    input_line_marker.scale.z = 0.1;
+    pub_spline_input.publish(input_line_marker);
     ros::spinOnce();
     rate.sleep();
 
@@ -78,7 +81,7 @@ auto main(int argc, char** argv) -> int {
     }
 
     visualization_msgs::Marker marker;
-    marker.header.seq = seq++;
+    marker.header.seq = output_seq++;
     marker.header.frame_id = "map";
     marker.header.stamp = ros::Time();
     marker.type = visualization_msgs::Marker::LINE_STRIP;
@@ -91,6 +94,10 @@ auto main(int argc, char** argv) -> int {
     marker.scale.y = 0.1;
     marker.scale.z = 0.1;
     pub_spline.publish(marker);
+
+    // while (ros::ok()) {
+
+    // }
 
     return 0;
 }
