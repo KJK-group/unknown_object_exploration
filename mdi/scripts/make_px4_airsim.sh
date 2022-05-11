8562ca72bd3279b8ead1ccf1e9121e3f2c0f5cb9
Jens <jens.jens@live.dk>
2022-05-11 20:34:21 +0200

    feat(launch): argument for external path for PX4-Autopilot

--- mdi/scripts/make_px4_airsim.sh
+++ mdi/scripts/make_px4_airsim.sh
@@ -3,7 +3,7 @@
 #cd ~/airsim_ws/PX4-Autopilot
 #make px4_sitl_default none_iris
 #catkin_ws="$(pwd)/../../.."
-terminator --new-tab -e "cd ${1}/PX4-Autopilot && make px4_sitl_default none_iris"
+terminator --new-tab -e "cd ${1} && make px4_sitl_default none_iris"
 #terminator_pid=$!
 #trap "kill -9 $terminator_pid" EXIT
 #trap "kill -9 $terminator_pid" SIGINT
